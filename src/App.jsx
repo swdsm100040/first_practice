2fd9e48fc81dc30e25f5ea16f84649d7218c2fb7
Rilesh Sharma <70507646+swdsm100040@users.noreply.github.com>
2023-07-12 16:29:49 +0530

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import Concept from "./componet/Concept";
+import Header from "./componet/Header";
+import "./index.css";
+
+function App() {
+    return (
+      <div>
+        <Header/>
+        <Concept/>
+      </div>
+    );
+}
+  
+  export default App;
